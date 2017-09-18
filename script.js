c181432e8ccf7a446df1392ff5a4544d67304904
Andi Zitney <zitneya@yahoo.com>
2017-09-17 22:10:48 -0700

    final updates

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
-  //getting album pictures
-  $.get('https://lit-fortress-6467.herokuapp.com/object', function(albums) {
+  //getting album pictures uploded wihtout duplicates
+  $.get('http://knowledgeable-destruction.surge.sh/test', function(albums) {
+    //for loop to make sure they do not repeat
     for (let i = 0; i < 3; i++) {
-      let coverArt = albums.results[Math.floor(Math.random() * 5)].cover_art;
+      if (i === 1) {
+        var y = Math.floor(Math.random() * 10);
+        while (y === x) {
+          y = Math.floor(Math.random() * 10);
+        }
+        pictures(i,y);
+      } else if (i === 2) {
+        z = Math.floor(Math.random() * 10);
+        while (z === x || z === y) {
+          z = Math.floor(Math.random() * 10);
+        }
+        pictures(i,z);
+      } else {
+        var x = Math.floor(Math.random() * 10);
+        pictures(i,x);
+      }
+    }
+    //function to upload pictures
+    function pictures(i, num){
+      let coverArt = albums.results[num].cover_art;
       let div = document.getElementsByClassName('pic')[i];
       $(div).append(`<img src="./images/${coverArt}" alt="album" style="width:230px;height:230px;">`);
     }
--- scripttwo.js
+++ scripttwo.js
@@ -1,18 +1,37 @@
 $(document).ready(function() {
-  //getting album pictures
-  $.get('https://lit-fortress-6467.herokuapp.com/object', function(albums) {
+  //getting album pictures across top
+  $.get('http://knowledgeable-destruction.surge.sh/test', function(albums) {
     for (let i = 0; i < albums.results.length; i++) {
       $('#playlist').append(`<li><img id="${albums.results[i].id}" src="./images/${albums.results[i].cover_art}" alt="album"></li>`);
 
       $(`#${albums.results[i].id}`).click(function(e) {
-        $.get('https://lit-fortress-6467.herokuapp.com/object', function(album) {
-
+        $.get('http://knowledgeable-destruction.surge.sh/test', function(album) {
           for (let i = 0; i < album.results.length; i++) {
 
             if (album.results[i].id == e.target.id) {
-              let p = document.createElement('p');
-              $(p).append(`${album.results[i].artist}: ${album.results[i].title}`);
-              $('#albumInfo').append(p);
+              //empty divs for next pic
+              $('#albumName').empty();
+              $('#allTracks').empty();
+              $('#selectedTrack').empty();
+              //create left div with album name and picture
+              let p1 = document.createElement('p');
+              $(p1).append(`Add Tracks From: <br> ${album.results[i].title}`);
+              $('#albumName').append(p1);
+              $('#albumName').append(`<img src="./images/${albums.results[i].cover_art}" alt="album">`);
+              //set up list of tracks
+              for (let j = 0; j < albums.results[i].tracks.length; j++) {
+                let pTrack = document.createElement('p');
+                pTrack.setAttribute('id', j);
+                $(pTrack).append(albums.results[i].tracks[j]);
+                $('#allTracks').append(pTrack);
+                //set click listener to each track and then be selected for the track list
+                $(`#${j}`).click(function(e) {
+                  let pSelect = document.createElement('p');
+                  $(pSelect).append(albums.results[i].tracks[e.target.id]);
+                  $('#selectedTrack').append(pSelect);
+                })
+              }
+
             }
 
           }
@@ -27,16 +46,15 @@ $(document).ready(function() {
   //submit button
   $('#submitBin').click(function() {
     $.post('https://lit-fortress-6467.herokuapp.com/post', function(data) {
-      $('#albumInfo').html(data)
+      $('#selectedTrack').html(data)
     });
   })
 
   //Clear Button
   $('#clear').click(function() {
-    $('#albumInfo').empty();
+    $('#albumName').empty();
+    $('#allTracks').empty();
+    $('#selectedTrack').empty();
   })
 
-    $.get('http://cooperative-jeans.surge.sh/test', function(test) {
-       $('body').append('hello');
-     })
 })
